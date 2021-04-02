d748b3172fea0fcfad581178e0e154139e466d8e
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-04-02 20:23:20 -0300

    Implementação de laço While
    Implementação da estrutura de repetção "while", juntamente com as variáveis rodada e numero_de_tentativas para informar o andamento da partida ao usuário.

--- part-one-introduction/jogos/adivinhacao.py
+++ part-one-introduction/jogos/adivinhacao.py
@@ -3,19 +3,26 @@ print("Bem vindo ao jogo da Adivinhação")
 print("********************************")
 
 numero_secreto = 42
-chute = int(input("Digite o seu chute: "))
-print(f"Você chutou o número {chute} ee...")
+rodada = 1
+numero_de_tentativas = 3
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+while rodada <= numero_de_tentativas:
+    print(f"Rodada {rodada} de {numero_de_tentativas}")
+    chute = int(input("Digite o seu chute: "))
+    print(f"Você chutou o número {chute} ee...")
 
-if acertou:
-    print("Você acertou! :)")
-else:
-    if maior:
-        print("Você errou... O seu chute foi maior que o número secreto :(")
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if acertou:
+        print("Você acertou! :)")
     else:
-        print("Você errou... O seu chute foi menor que o número secreto :(")
+        if maior:
+            print("Você errou... O seu chute foi maior que o número secreto :(")
+        else:
+            print("Você errou... O seu chute foi menor que o número secreto :(")
+
+    rodada += 1
 
 print("Fim!")
