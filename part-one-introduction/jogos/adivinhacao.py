e19cf8629870935fd80dae0ffed5e8961716c751
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-04-03 19:54:41 -0300

    Redução dos valores possíveis para chute e parada ao acertar o número mágico

--- part-one-introduction/jogos/adivinhacao.py
+++ part-one-introduction/jogos/adivinhacao.py
@@ -7,8 +7,12 @@ numero_de_tentativas = 3
 
 for rodada in range(1, numero_de_tentativas + 1):
     print(f"Rodada {rodada} de {numero_de_tentativas}")
-    chute = int(input("Digite o seu chute: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
     print(f"Você chutou o número {chute} ee...")
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100.")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -16,6 +20,7 @@ for rodada in range(1, numero_de_tentativas + 1):
 
     if acertou:
         print("Você acertou! :)")
+        break
     else:
         if maior:
             print("Você errou... O seu chute foi maior que o número secreto :(")
