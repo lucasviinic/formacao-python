fe6199e024e181e60377b8a21a83e40f260fee26
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-05-01 12:10:03 -0300

    Manipulação de Strings

---
+++ manipulando-strings/extrator-url/extrator_cep.py
@@ -0,0 +1,9 @@
+import re
+
+endereco = "Rua das Flores, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120"
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
+busca = padrao.search(endereco) #Match
+
+if busca:
+    cep = busca.group()
+    print(cep)
