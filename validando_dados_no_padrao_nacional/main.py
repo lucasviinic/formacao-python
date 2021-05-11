dfdb94310870d6f27b98524f356f468294ff7cd7
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-05-11 18:44:07 -0300

    Validando CNPJ e construindo uma Factory
    - Como utilizar mais uma classe da validate_docbr.
    - O que é o padrão de projeto Factory.
    - Como e quando implementar uma Factory em nosso código.

--- validando_dados_no_padrao_nacional/main.py
+++ validando_dados_no_padrao_nacional/main.py
@@ -1,4 +1,10 @@
-from Cpf import Cpf
+from cpf_cnpj import Documento
+from validate_docbr import CNPJ
 
-cpf_um = Cpf("48573348503")
-print(cpf_um)
+exemplo_cnpj = "35379838000112"
+exemplo_cpf = "32007832062"
+#cnpj = CNPJ()
+#print(cnpj.validate(exemplo_cnpj))
+#cpf_um = Cpf("48573348503")
+documento_um = Documento.cria_documento(exemplo_cnpj)
+print(documento_um)
