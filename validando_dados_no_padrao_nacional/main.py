f4c016374ad9d173c99220aaf4a0a840a2820662
Lucas Vinicius <lucas.vinicius@trukah.com.br>
2021-05-12 18:37:15 -0300

    Validando telefones com expressões regulares
    - O que são expressões regulares;
    - Como construir padrões e encontra-los dentro de textos;
    - Como validar com expressões regulares;
    - Como criar máscaras com expressões regulares.

--- validando_dados_no_padrao_nacional/main.py
+++ validando_dados_no_padrao_nacional/main.py
@@ -1,10 +1,5 @@
-from cpf_cnpj import Documento
-from validate_docbr import CNPJ
+from TelefonesBr import TelefonesBr
 
-exemplo_cnpj = "35379838000112"
-exemplo_cpf = "32007832062"
-#cnpj = CNPJ()
-#print(cnpj.validate(exemplo_cnpj))
-#cpf_um = Cpf("48573348503")
-documento_um = Documento.cria_documento(exemplo_cnpj)
-print(documento_um)
+numero = "558196760893"
+telefone = TelefonesBr(numero)
+print(telefone)
