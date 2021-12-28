42009b6355476d0581cbe64294fb28953721e45f
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-12-28 19:05:01 -0300

    Uso de dublês

--- duble-de-testes-python/colecao/livros.py
+++ duble-de-testes-python/colecao/livros.py
@@ -1,6 +1,10 @@
 def consultar_livros(autor):
-    preparar_dados_para_requisicao(autor)
+    dados = preparar_dados_para_requisicao(autor)
+    obter_url("https://buscador", dados)
     return ""
 
 def preparar_dados_para_requisicao(autor):
+    pass
+
+def obter_url(url, dados):
     pass
--- duble-de-testes-python/tests/test_livros.py
+++ duble-de-testes-python/tests/test_livros.py
@@ -12,6 +12,14 @@ def test_consultar_livros_chama_preparar_dados_para_requisicao_uma_vez_e_com_os_
         consultar_livros("Agatha Christie")
         duble.assert_called_once_with("Agatha Christie")
 
+def test_consultar_livros_chama_a_funcao_obter_url_usando_como_parametro_o_retorno_de_preparar_dados_para_requisicao():
+    with patch("colecao.livros.preparar_dados_para_requisicao") as duble_preparar:
+        dados = {"autor": "Agatha Christie"}
+        duble_preparar.return_value = dados
+        with patch("colecao.livros.obter_url") as duble_obter_url:
+            consultar_livros("Agatha Christie")
+            duble_obter_url.assert_called_once_with("https://buscador", dados)
+
 
 
 
